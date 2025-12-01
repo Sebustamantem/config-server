558c655c520b4ed6824fc98ff84503417e6c18c0
Sebastian Bustamante <Se.bustamantem@duocuc.cl>
2025-11-30 23:55:29 -0300

    Cmabios y mas cambios

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,8 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.cloud:spring-cloud-config-server")
 
+
+
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
